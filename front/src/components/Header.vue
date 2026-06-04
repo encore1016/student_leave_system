791e26efc45aba523a081cafa896e71a01739162
litianjiao <2679788207@qq.com>
2026-06-04 11:10:00 +0800

    优化导航栏与菜单结构

---
+++ front/src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <div class="title">
+      学生请假管理系统
+    </div>
+
+    <div class="user-info">
+      欢迎你，{{ username }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      username: "admin"
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  height: 60px;
+  background-color: #409EFF;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  color: white;
+}
+
+.title {
+  font-size: 22px;
+  font-weight: bold;
+}
+
+.user-info {
+  font-size: 14px;
+}
+</style>
---
+++ front/src/components/Menu.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="menu">
+    <ul>
+      <li @click="goPage('/home')">首页</li>
+      <li @click="goPage('/apply')">请假申请</li>
+      <li @click="goPage('/record')">请假记录</li>
+      <li @click="goPage('/user')">个人中心</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+
+  methods: {
+    goPage(path) {
+      this.$router.push(path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.menu {
+  width: 200px;
+  height: 100vh;
+  background-color: #2f4050;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  height: 50px;
+  line-height: 50px;
+  color: white;
+  padding-left: 20px;
+  cursor: pointer;
+}
+
+li:hover {
+  background-color: #409EFF;
+}
+</style>
